6e99aa5a5783a7810bf3c0dab9d7e63ff6e6e971
PoSh82 <14829848+PoSh82@users.noreply.github.com>
2023-08-09 23:35:43 +0200

    Update GenderRemoval.js

--- GenderRemoval.js
+++ GenderRemoval.js
@@ -12,15 +12,33 @@
     'use strict';
 
     // Define the regular expression pattern
-    var regexPattern = /(Innen|:Innen|\*Innen|\.Innen|_Innen|\/Innen|(Innen)|-Innen|\|Innen|\\Innen|;Innen|:innen|\*innen|\.innen|_innen|\/innen|(Innen)|-innen|\|innen|\\innen|;innen|·Innen|·innen|:In|\*In|_In|\/In|\|In|;In|·In|:in|\*in|_in|\/in|\|in|;in|·in|(?<=\w\b(?!Linked))In\b)\b/g;
+    var regexPattern = /(Innen|:Innen|\*Innen|\.Innen|_Innen|\/Innen|(Innen)|-Innen|\|Innen|\\Innen|;Innen|:innen|\*innen|\.innen|_innen|\/innen|(Innen)|-innen|\|innen|\\innen|;innen|·Innen|·innen|:In|\*In|_In|\/In|\|In|;In|·In|:in|\*in|_in|\|in|;in|·in|(?<=\w\b(?!Linked))In\b)\b/g;
+    var regexPattern2 = /Menschin/gi;
+    var regexPattern3 = /Personin/gi;
+    var regexPattern4 = /Mitgliederin/gi;
+    var regexPattern5 = /Menschinnen/gi;
+    var regexPattern6 = /Personinnen/gi;
+    var regexPattern7 = /Mitgliederinnen/gi;
 
     // Define the replacement text
     var replacementText = '';
+    var replacementText2 = 'Mensch';
+    var replacementText3 = 'Person';
+    var replacementText4 = 'Mitglied';
+    var replacementText5 = 'Menschen';
+    var replacementText6 = 'Personen';
+    var replacementText7 = 'Mitglieder';
 
     // Remove matching words
     function replaceText(node) {
         if (node.nodeType === Node.TEXT_NODE) {
             node.textContent = node.textContent.replace(regexPattern, replacementText);
+            node.textContent = node.textContent.replace(regexPattern2, replacementText2);
+            node.textContent = node.textContent.replace(regexPattern3, replacementText3);
+            node.textContent = node.textContent.replace(regexPattern4, replacementText4);
+            node.textContent = node.textContent.replace(regexPattern5, replacementText5);
+            node.textContent = node.textContent.replace(regexPattern6, replacementText6);
+            node.textContent = node.textContent.replace(regexPattern7, replacementText7);
         } else if (node.nodeType === Node.ELEMENT_NODE) {
             for (var i = 0; i < node.childNodes.length; i++) {
                 replaceText(node.childNodes[i]);
